cd988da41401a11bb3359b3dd5b3f38a15e3ea5f
sagarharale9 <sagarharale9@gmail.com>
2025-06-22 16:53:10 +0530

    added method and logs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        String input ="three thousand twenty three rupees";
+        System.out.println("Started Main");
+        wordsToNumConvertor("three thousand twenty four rupees");
+        System.out.println("End Main");
+
+    }
+
+    public static void wordsToNumConvertor(String input){
+        System.out.println("Main :: wordsToNumConvertor");
         WordsToNumConvertor convertor = new WordsToNumConvertor();
         int result = convertor.wordsToNumConvert(input);
-        System.out.println(result);
+        System.out.println("Input: "+input+ " \nOutput: "+result);
 
     }
 
